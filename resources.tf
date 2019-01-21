b23a74a098330e79a04ce0c959f52f78d40e072d
APillari <anil_p02@yahoo.com>
2019-01-21 16:48:53 -0500

    no message

--- resources.tf
+++ resources.tf
@@ -1,34 +1,35 @@
 resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames =true
-  enable_dns_support = true
-  tags{
-      Name="terraform-aws-vpc-example-2"
+  cidr_block           = "${var.vpc-cidr-block}"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags {
+    Name = "terraform-aws-vpc-example-2"
   }
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block="${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
-  vpc_id="${aws_vpc.environment-example-two.id}",
+  cidr_block        = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.environment-example-two.id}"
   availability_zone = "us-east-2a"
 }
 
 resource "aws_subnet" "subnet2" {
-  cidr_block="${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}",
-  vpc_id = "${aws_vpc.environment-example-two.id}",
-  availability_zone="us-east-2b"
+  cidr_block        = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-east-2b"
 }
 
 resource "aws_security_group" "subnet-security" {
-  vpc_id= "${aws_vpc.environment-example-two.id}"
-  ingress
-  {
-      cidr_blocks = [
-          "${aws_vpc.environment-example-two.cidr_block}"
-      ]
+  vpc_id = "${aws_vpc.environment-example-two.id}"
 
-      from_port = 80
-      to_port =80
-      protocol = "tcp"
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}",
+    ]
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,4 @@
+variable "vpc-cidr-block" {
+  type = "string"
+  default = "10.0.0.0/16"
+}
